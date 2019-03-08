3c2672754748ccf254494e62660a6bc4e761e5c8
Krymancer <junior.nascm@gmail.com>
2019-03-08 01:50:21 -0300

    Frist commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Rock-Papper-Scissors
+
+A simple as possible rock-paper-scissors made with pude JavaScript, poor designed in css in html.
+
+- No libraries
+- No third-party uses
---
+++ app.js
@@ -0,0 +1,88 @@
+let playerScore = 0;
+let computerScore = 0;
+
+const playerSCore_a = document.getElementById("playerScore");
+const computerScore_a = document.getElementById("computerScore");
+const playerOption_a = document.getElementById("playerOption");
+const computerOption_a = document.getElementById("computerOption");
+const gameStatus_a = document.getElementById("gameStatus");
+const r_div = document.getElementById("rock");
+const p_div = document.getElementById("papper");
+const s_div = document.getElementById("scissors");
+
+main();
+
+function getComputerOption(){
+    let options = ['r','p','s'];
+    return options[Math.floor(Math.random()*3)];
+}
+
+function main(){
+    r_div.addEventListener('click',function(){
+        game('r');
+    });
+
+    p_div.addEventListener('click',function(){
+        game('p');
+    });
+
+    s_div.addEventListener('click',function(){
+        game('s');
+    }); 
+}
+
+function game(playerOption){
+
+    let computerOption = getComputerOption();
+
+    switch (playerOption + computerOption) {
+        case 'rs':
+        case 'pr':
+        case 'sp':{
+            playerScore++;
+            gameStatus_a.innerHTML = "Venceu";
+            playerOption_a.innerHTML = transformToWord(playerOption);
+            computerOption_a.innerHTML = transformToWord(computerOption);
+            playerSCore_a.innerHTML = playerScore;
+            computerScore_a.innerHTML = computerScore;
+            break;
+        }
+        case 'rp':
+        case 'ps':
+        case 'sr':{
+            computerScore++;
+            gameStatus_a.innerHTML = "Perdeu";
+            playerOption_a.innerHTML = transformToWord(playerOption);
+            computerOption_a.innerHTML = transformToWord(computerOption);
+            playerSCore_a.innerHTML = playerScore;
+            computerScore_a.innerHTML = computerScore;
+            break;
+        }
+        case 'rr':
+        case 'pp':
+        case 'ss':{
+            gameStatus_a.innerHTML = "Empatou";
+            playerOption_a.innerHTML = transformToWord(playerOption);
+            computerOption_a.innerHTML = transformToWord(computerOption);
+            playerSCore_a.innerHTML = playerScore;
+            computerScore_a.innerHTML = computerScore;
+            break;
+        }
+    }
+}
+
+function transformToWord(option){
+    if(option === 'r') return "Pedra";
+    if(option === 'p') return "Papel";
+    if(option === 's') return "Tesoura";
+}
+
+// console.log(playerSCore_a);
+// console.log(computerScore_a);
+// console.log(playerOption_a);
+// console.log(computerOption_a);
+// console.log(gameStatus_a);
+// console.log(r_div);
+// console.log(p_div);
+// console.log(s_div);
+// console.log(computerOption());
